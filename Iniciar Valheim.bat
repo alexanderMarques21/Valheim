578e6c5647df33ea10b97f986d6130db69c070cb
Alex <aleiftm@gmail.com>
2021-02-17 22:56:52 -0300

    Salvo novo progresso

--- Iniciar Valheim.bat
+++ Iniciar Valheim.bat
@@ -3,15 +3,17 @@ set HOME=%USERPROFILE%
 git pull origin master
 cls
 pause
-cd C:\Users\alex\Desktop\Valheim
+
+
+cd C:\Users\alex\Desktop\Valheim\
+
 valheim.exe
+
 cls
 echo "Nao fecha essa janela"
 
 cd %HOME%/AppData\LocalLow\IronGate\Valheim
-git add . 
-git commit -m "Salvo novo progresso"
+git commit -am "Salvo novo progresso"
 git push origin master
 
-cls
-exit
+pause
